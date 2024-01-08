be7aa4d96550d7fb044330229c72b1a0f5660d3f
PraveenNandha <praveennandha33@gmail.com>
2024-01-08 21:25:48 +0530

    Added End to end automation for add place,update place ,validate the new adddress using get

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static  org.hamcrest.Matchers.*;
 
@@ -8,27 +9,50 @@ public class Basics {
     public  static  void main(String[] args)
     {
         //Setting the base URI
+//        RestAssured.baseURI="https://rahulshettyacademy.com/";
+//        //Addding the query param , header and body
+//        //log().all() used to print the logs .This will log the req
+//        //Body accepts only json
+//        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+//                .body("{\n" +
+//                        "  \"location\": {\n" +
+//                        "    \"lat\": -38.383494,\n" +
+//                        "    \"lng\": 33.427362\n" +
+//                        "  },\n" +
+//                        "  \"accuracy\": 50,\n" +
+//                        "  \"name\": \"Frontline house\",\n" +
+//                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+//                        "  \"address\": \"29, side layout, cohen 09\",\n" +
+//                        "  \"types\": [\n" +
+//                        "    \"shoe park\",\n" +
+//                        "    \"shop\"\n" +
+//                        "  ],\n" +
+//                        "  \"website\": \"https://rahulshettyacademy.com\",\n" +
+//                        "  \"language\": \"French-IN\"\n" +
+//                        "}\n")
+//                //in post parameter we should  give the resource
+//                .when().post("maps/api/place/add/json")
+//                //Adding assertion for status code
+//                .then().log().all().assertThat().statusCode(200)
+//                //Validating the scope in response body is app
+//                .body("scope",equalTo("APP"))
+//                //Validating the server name in response header
+//                .header("server","Apache/2.4.52 (Ubuntu)");
+//
+//        System.out.println("Testing the testingWithPayload method");
+//        testingWithPayload();
+//        learnExtractionAndJSONPath();
+        AddUpdateGetE2ETesting();
+    }
+
+    public static void testingWithPayload(){
         RestAssured.baseURI="https://rahulshettyacademy.com/";
         //Addding the query param , header and body
         //log().all() used to print the logs .This will log the req
         //Body accepts only json
         given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"https://rahulshettyacademy.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}\n")
+                //Fetched the body from payload class
+                .body(payload.addPlace())
                 //in post parameter we should  give the resource
                 .when().post("maps/api/place/add/json")
                 //Adding assertion for status code
@@ -37,5 +61,62 @@ public class Basics {
                 .body("scope",equalTo("APP"))
                 //Validating the server name in response header
                 .header("server","Apache/2.4.52 (Ubuntu)");
+    }
+
+    public  static void learnExtractionAndJSONPath(){
+        RestAssured.baseURI="https://rahulshettyacademy.com/";
+        String res=given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                //Fetched the body from payload class
+                .body(payload.addPlace())
+                //in post parameter we should  give the resource
+                .when().post("maps/api/place/add/json")
+                //Adding assertion for status code
+                .then().assertThat().statusCode(200)
+                //Validating the scope in response body is app
+                .body("scope",equalTo("APP"))
+                //Validating the server name in response header
+                .header("server","Apache/2.4.52 (Ubuntu)").extract().response().asString();
+        //This method will take string as i/p convert it into json and supports to parse json with diff methods
+        JsonPath js=new JsonPath(res);
+        //inside the getstring method we need to give the path for the node
+        String placeId=js.getString("place_id");
+        System.out.println("PlaceId="+placeId);
+
+    }
+
+    public static void AddUpdateGetE2ETesting(){
+        //Add Place
+        RestAssured.baseURI="https://rahulshettyacademy.com/";
+        String response=given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body(payload.addPlace())
+                .when().post("maps/api/place/add/json")
+                .then().assertThat().statusCode(200)
+                .extract().response().asString();
+        JsonPath js=new JsonPath(response);
+        String placeid=js.getString("place_id");
+        System.out.println("Added place successfully");
+        //Update Address - PUT
+        String newAddress="114,Pollachi, Coimbatore";
+        given().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body("{\n" +
+                        "\"place_id\":\""+placeid+"\",\n" +
+                        "\"address\":\""+newAddress+"\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}\n").
+                when().put("maps/api/place/update/json").
+                then().log().all().assertThat().body("msg",equalTo("Address successfully updated")).statusCode(200);
+
+        //Fetch the latest address and validate the new address is coming in response -Get
+        String fetchGetRes=given().log().all().queryParam("key","qaclick123").and().queryParam("place_id",placeid).
+                when().get("maps/api/place/get/json").then().log().all().assertThat().statusCode(200).extract().response().asString();
+        JsonPath actualGetResponse=new JsonPath(fetchGetRes);
+        String actualAddress= actualGetResponse.getString("address");
+        System.out.println("Actual Address="+actualAddress);
+
+
+
+
+
+
     }
 }
